bf63e533e271b2588a78853c5887a13bcbb2db22
Sumit Kumar <sumittechboy@yahoo.com>
2025-10-10 06:15:33 +0530

    js practice files add (variables in js)

---
+++ js_practice/variables.js
@@ -0,0 +1,18 @@
+// variable using Var keyword
+
+var x = 10;
+
+console.log(x);
+console.log(typeof(x)) //this line of code will display the datatype of the variable
+
+
+//Note - Null datatype is defined as an object in javascript.
+
+// array datatype 
+
+var arr = [20, 50, 30, 'string']
+
+console.log(arr)
+console.log(typeof(arr))
+
+// array datatype is aslso an object in javascript.
