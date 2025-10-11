c5d2e17a015a73cd822acfe31c54a03694cf632a
Sumit Kumar <sumittechboy@yahoo.com>
2025-10-11 18:09:13 +0530

    leet code question practice added

---
+++ public/practice_sessions/js_practice/leetCode.js
@@ -0,0 +1,20 @@
+// alternate sum problem
+
+// console.log('Alternate sum')
+
+// const nums = [1,2,3,4,5];
+
+// const alternatingSum = function(nums) {
+//   let sum = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     sum += (i % 2 === 0) ? nums[i] : -nums[i];
+//   }
+//   return sum;
+// };
+
+// console.log(alternatingSum(nums)); 
+
+var nums = [2,7,11,15];
+var target = 9;
+
+for(i = 0; i<nums.length; i++)
